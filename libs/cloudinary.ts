1ef371ce111af5842db03fc27e6591095e40c92b
KL0CKERS09 <fredobendal0808@gmail.com>
2025-05-02 13:06:56 -0800

    Fix: added 'use client' to user report page

--- libs/cloudinary.ts
+++ libs/cloudinary.ts
@@ -1,4 +1,5 @@
-import cloudinary from "cloudinary";
+import { v2 as cloudinary } from 'cloudinary';
+import streamifier from 'streamifier';  // Import streamifier to convert buffer to stream
 
 // Set up Cloudinary configuration
 cloudinary.config({
@@ -10,8 +11,18 @@ cloudinary.config({
 // Function to upload an image to Cloudinary
 export const uploadImage = async (image: File) => {
     try {
-        const res = await cloudinary.uploader.upload(image.path); // Upload image to Cloudinary
-        return res.secure_url; // Return the image URL
+        const arrayBuffer = await image.arrayBuffer();
+        const buffer = Buffer.from(arrayBuffer);
+
+        // Create a stream from the buffer
+        const stream = streamifier.createReadStream(buffer);
+
+        // Upload the stream to Cloudinary
+        const res = await cloudinary.uploader.upload(stream, {
+            resource_type: "auto", // Automatically detect the file type (image, video, etc.)
+        });
+
+        return res.secure_url; // Return the secure URL of the uploaded image
     } catch (error) {
         console.error("Error uploading image to Cloudinary:", error);
         throw new Error("Failed to upload image");
